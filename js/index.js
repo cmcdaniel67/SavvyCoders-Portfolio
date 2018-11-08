9c0cafaffc2724762a8990c197138531d60f1f24
Carol McDaniel <nannayarn@gmail.com>
2018-11-08 17:19:44 -0600

    paragraph

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ var firstName = prompt("Hi there! What's your first name?");
 var lastName = prompt("What's your last name?");
 var output = document.querySelector("#greeting");
 
-var checkName = function(){
+function checkName(){
   if(firstName && firstName != "null" && lastName != 'null')  {
     output.innerHTML = `<p>Thanks for visiting ${firstName} ${lastName}.</p>`; 
 
