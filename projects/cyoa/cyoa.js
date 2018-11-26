f99aa8604163e8a04a851fb3a24b903b002d4f69
pet <nannayarn @gmail.com>
2018-11-26 17:41:15 -0600

    add changes

--- projects/cyoa/cyoa.js
+++ projects/cyoa/cyoa.js
@@ -1,21 +1,16 @@
-var response = prompt("You walk into a room with a chair and a window. Type 'sit' to sit in the chair, type 'gaze' to gaze wistfully out the window and sigh");
+var response = prompt("You walk into a meadow with an apple tree and a swing. Type 'get' to pick an apple, type 'swing' to swing from the tree");
 
-if(response === "sit"){
-    response = prompt("Here's a new prompt, with new options");
-} else if (response === "gaze") {
-    response = prompt("Here's a new prompt, with new options");
-} else {
-    alert("Please type in a valid input! Refresh the page to try again.");
-}
-
-
-
----------*******var response = prompt("You walk into a meadow with a tree and a swing. Type 'pick' to pick an apple, type 'swing' to swing from the tree");
-
-if(response === "pick"){
-    response = prompt("There's a tree. Type 'hungry' to get an apple from the tree, type 'munch' to eat the apple");
+if(response === "get"){
+    response = prompt("There's a tree. Type 'peel' to peel an apple, type 'munch' to eat the apple");
 } else if (response === "swing") {
     alert("Now you are going to starve."); 
 } else 
     alert("Now you have a tummy ache.");
-} 
+//handle "peel" and "munch"
+
+if(response === "peel"){
+    alert("The apple is ripe")
+} else if (response === "munch") {
+    alert("You are no longer hungry")
+}
+    
